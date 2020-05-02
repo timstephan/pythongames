6dd552e962fbd758aa1e81493aea817e4c78b610
TimS <timstephan@users.noreply.github.com>
2020-05-02 16:49:40 +0200

    Add menu to the game

---
+++ menu.py
@@ -0,0 +1,31 @@
+# Menu to start the games
+import os
+
+print('''
+                              mm                                                                                           
+*@@@***@@m              @@   @@@                                     mm@***@m@                                             
+  @@   *@@m             @@    @@                                   m@@*     *@                                             
+  @@   m@@ *@@*   *@@*@@@@@@  @@@@@@@m    m@@*@@m *@@@@@@@@m       @@*       *  m@*@@m  *@@@@@@@@m@@@@@m    mm@*@@  m@@*@@@
+  @@@@@@@    @@   m@    @@    @@    @@   @@*   *@@  @@    @@       @!          @@   @@    @@    @@    @@   m@*   @@ @@   **
+  @@          @@ m!     @@    @@    @!   @@     @@  @!    @@       @!m    *@@@@ m@@@!@    !@    @@    @@   !@****** *@@@@@m
+  @!           @@!      @!    @!    @!   @@     !@  @!    !@       *!@m     @@ @!   !@    !@    !@    @@   !@m    m      @@
+  @!           @!!      !!    !!    !!   !@     !!  !!    !!       !!!    *!@!! !!!!:!    !!    !!    !!   !!****** *!   @!
+  !!           !!:      !!    !:    !:   !!!   !!!  !!    !!       *:!!     !! !!   :!    :!    :!    !!   :!!      !!   !!
+:!:!:          !!       ::: :: :   : : :  : : : : : :::  :!: :       ::: : ::  :!: : !: : :!:  :::   ::!:   : : ::  : :!:  
+             ::!                                                                                                           
+           :::                                                                                                             
+''')
+
+print("Please choose one game: ")
+print("Type number to start game.")
+print("1. tic tac toe")
+print("2. Hangman the Game")
+print("3. Exit")
+i = int(input())
+
+if i == 1:
+    os.system('python tictactoelastbackup.py')
+if i == 2:
+    os.system('python Hangman_the_game.py')
+if i == 3:
+    print("Bye! See you soon.")
